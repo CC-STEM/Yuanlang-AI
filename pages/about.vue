4f33cd1f3f3aa693a753dfeb7a7e743d957bce1e
huangjingyuan <837317210@qq.com>
2024-10-24 17:08:21 +0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <p>此页面将显示在 /about 路由。</p>
+  </section>
+  <el-button @click="() => { console.log(111); ElMessage.success('hello') }">button</el-button>
+  <ElButton @click="() => { console.log(222) }" :icon="ElIconEditPen" type="success">button</ElButton>
+  <LazyElButton type="warning">lazy button</LazyElButton>
+</template>
