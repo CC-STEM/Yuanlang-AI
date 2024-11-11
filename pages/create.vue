a655ed341f3af782cdab05c2d7a1ac9dd7698f80
huangjingyuan <837317210@qq.com>
2024-11-11 12:36:46 +0800

    fix: fix

--- pages/create.vue
+++ pages/create.vue
@@ -9,7 +9,7 @@
             <span tabindex="2" class="title-btn">我的创作</span>
           </div>
         </div>
-        <div class="w-full mb-[40px]">
+        <div class="w-full mb-[20px]">
           <div class="h-[24px] text-left text-white">* 模型选择</div>
           <div class="h-[112px] flex justify-start items-center pt-[4px] pb-[4px]">
             <div :tabindex="index" class="model-item flex flex-col items-center text-[12px] justify-around"
@@ -17,6 +17,19 @@
               <span class="text-white ">{{ item.name }}</span>
               <span class="text-[#b1b5c4]">{{ item.desc }}</span>
             </div>
+          </div>
+        </div>
+        <div class="w-full mb-[20px]">
+          <div class="w-full text-left text-white mb-[8px]">* 模型主题选择</div>
+          <div class="w-full mb-[20px]">
+            <el-radio-group v-model="curModelStyle" size="default">
+              <el-radio-button v-for="item in modelStyleList" :label="item" :value="item" />
+            </el-radio-group>
+          </div>
+          <div class="w-full flex flex-wrap">
+            <div class="model-theme-item flex justify-center items-center text-white" :tabindex="index"
+              v-for="(item, index) in modelThemes">
+              {{ item.name }}</div>
           </div>
         </div>
         <div class="w-full mb-[40px]">
@@ -29,7 +42,7 @@
             <el-input :maxlength="1000" :autosize="{ minRows: 4 }" :show-word-limit="true" v-model="promptStr"
               style="width: 100%" :rows="4" type="textarea" resize="none" placeholder="请输入咒语" />
           </div>
-          <div class="w-full">
+          <div class="w-full mb-[30px]">
             <div class="w-full text-left text-white mb-[8px]">* 画面大小</div>
             <div class="w-full flex h-[94px] mb-[20px]">
               <div :tabindex="index" v-for="(item, index) in aspectRatios"
@@ -81,8 +94,136 @@ const aspectRatios = ref([{
   size: '9:16'
 }])
 
+const modelThemes = ref([
+  {
+    "name": "汽车设计"
+  },
+  {
+    "name": "服装模型"
+  },
+  {
+    "name": "儿童摄影"
+  },
+  {
+    "name": "玩具设计"
+  },
+  {
+    "name": "影视（架空）"
+  },
+  {
+    "name": "私人影像"
+  },
+  {
+    "name": "室内设计XL"
+  },
+  {
+    "name": "影视近代"
+  },
+  {
+    "name": "艺术插画"
+  },
+  {
+    "name": "NJXL V6"
+  },
+  {
+    "name": "加密艺术"
+  },
+  {
+    "name": "头像"
+  },
+  {
+    "name": "植物花卉"
+  },
+  {
+    "name": "纹样"
+  },
+  {
+    "name": "家居设计"
+  },
+  {
+    "name": "MJXL V6"
+  },
+  {
+    "name": "国漫"
+  },
+  {
+    "name": "NJXL V5"
+  },
+  {
+    "name": "游戏设计"
+  },
+  {
+    "name": "工艺美术"
+  },
+  {
+    "name": "宠物摄影"
+  },
+  {
+    "name": "美食"
+  },
+  {
+    "name": "壁纸"
+  },
+  {
+    "name": "产品美学"
+  },
+  {
+    "name": "汉服"
+  },
+  {
+    "name": "插画"
+  },
+  {
+    "name": "文字"
+  },
+  {
+    "name": "室内设计"
+  },
+  {
+    "name": "MJ"
+  },
+  {
+    "name": "二次元"
+  },
+  {
+    "name": "风景"
+  },
+  {
+    "name": "科幻"
+  },
+  {
+    "name": "女性漫"
+  },
+  {
+    "name": "中国风"
+  },
+  {
+    "name": "儿童"
+  },
+  {
+    "name": "奇幻"
+  },
+  {
+    "name": "游戏"
+  },
+  {
+    "name": "艺术"
+  },
+  {
+    "name": "珠宝"
+  },
+  {
+    "name": "动物"
+  },
+  {
+    "name": "通用"
+  }
+])
+
 const resolutions = ref(['1024*1024', '1360*1360', '2048*2048'])
 
+const modelStyleList = ref(['收藏', '推荐', 'MJ', '二次元', '真人', '科幻', '儿童', '设计', '画风', '中国风', '风景'])
+const curModelStyle = ref('')
 </script>
 
 <style scoped lang="scss">
@@ -164,5 +305,24 @@ const resolutions = ref(['1024*1024', '1360*1360', '2048*2048'])
 
 .resolute-item:focus {
   border: 2px solid rgb(177, 181, 196);
+}
+
+.model-theme-item {
+  // flex: 1;
+  width: 13%;
+  height: 36px;
+  background: #23262f;
+  margin: 3px;
+  border-radius: 90px;
+  font-size: 12px;
+}
+
+.model-theme-item:focus {
+  border: 2px solid rgb(177, 181, 196);
+}
+
+:deep(.el-radio-button__inner) {
+  border: 1px solid rgb(35, 38, 47);
+  background: none;
 }
 </style>
