cd83bfd430b6175aa184b281370fb0bb96db69eb
xiangyi123456 <13974509+xiangyi123456@user.noreply.gitee.com>
2025-04-11 17:29:14 +0800

    feat： api fix

--- composables/member.ts
+++ composables/member.ts
@@ -1,23 +1,23 @@
 export const getPackage = async () => {
-  return await commoneFetch<any>(`internal/member/package/all`, {
+  return await commoneFetch<any>(`/internal/member/package/all`, {
     method: "GET",
   });
 };
 
 export const findByUserId = async () => {
-  return await commoneFetch<any>(`internal/member/findByUserId`, {
+  return await commoneFetch<any>(`/internal/member/findByUserId`, {
     method: "GET",
   });
 };
 
 export const freeRegister = async () => {
-  return await commoneFetch<any>(`internal/member/freeRegister`, {
+  return await commoneFetch<any>(`/internal/member/freeRegister`, {
     method: "GET",
   });
 };
 
 export const getWxPay = async (params: any) => {
-  return await commoneFetch<any>(`internal/api/wxPay/native/pay`, {
+  return await commoneFetch<any>(`/internal/api/wxPay/native/pay`, {
     method: "POST",
     body: params,
   });
@@ -25,7 +25,7 @@ export const getWxPay = async (params: any) => {
 
 export const queryOrder = async (orderNo: any) => {
   return await commoneFetch<any>(
-    `internal/api/wxPay/native/queryOrder/${orderNo}`,
+    `/internal/api/wxPay/native/queryOrder/${orderNo}`,
     {
       method: "GET",
     }
@@ -33,7 +33,7 @@ export const queryOrder = async (orderNo: any) => {
 };
 
 export const getAliPay = async (params: any) => {
-  return await commoneFetch<any>(`internal/api/aliPay/native/pay`, {
+  return await commoneFetch<any>(`/internal/api/aliPay/native/pay`, {
     method: "POST",
     body: params,
   });
